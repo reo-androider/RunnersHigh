a25c1862c27f9a50fccab8e51247551215c69b68
github lion-king-IT <reo.runningg@gmail.com>
2021-01-06 23:03:08 +0900

    インスタみたいな下部のナビゲーションボタンを実装しました！

--- app/src/main/java/com/reo/running/runnershigh/MainActivity.kt
+++ app/src/main/java/com/reo/running/runnershigh/MainActivity.kt
@@ -1,21 +1,37 @@
 package com.reo.running.runnershigh
-
 /**
+ * 目的は？
+ * ・とにかく、ランニングを習慣にしている人、あるいはしようとしている人がランニング出来るぞとワクワクするような工夫を凝らしたアプリを作る
+ * 工夫とは？
+ * ・時間や距離の数値を他に例えてみる、（ここが目玉機能） 距離だったらそれを縦にしたらどこまでいけるのか？計測する
+ * →電車だったらどこまで行けるのか？
+ * →飛行機だったら？
+ * →水中だったら？
+ * ・ランニング中にひらめいたアイデアをためる
+ * ・ツイートで○○さんは△△まで走りました！という報告ツイート面白そう（オゾン層とかね（笑）エベレスト10往復）
+ * ・天気予報も見れるといいかも！
+ * ・ランニングに関する改善点を毎日開くたびに教えてくれる AndroidStudio的に
+ * ・画面を開くとラジオ体操のスタンプみたいにスタンプラリーのUIが表示される
  * 処理概要具体化
- * フラグメントで、上部画面にタイトルバー
- * 下部画面に操作画面（ホーム画面、写真投稿、検索、プロフィール（高さ）画像はキャラクターを選択できると良いね）
+ * 1月中
+ * 下部ボタンを押すとUI切り替え
  * firebase 認証
  * firebase DB
  * 位置情報取得
  * 移動距離取得
  */
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        //setupWithNavController(bottom_navigation_test,)
     }
 }
