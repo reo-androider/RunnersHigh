40e1484173b93c6afe5faf4b7f5f898d2cac890e
日髙 尚輝 <naoki.h@arsaga.jp>
2021-01-08 23:29:43 +0900

    modify

--- app/src/main/java/com/reo/running/runnershigh/MainActivity.kt
+++ app/src/main/java/com/reo/running/runnershigh/MainActivity.kt
@@ -17,8 +17,9 @@ package com.reo.running.runnershigh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +27,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        setupWit
+        setupWithNavController(bottom_navigation, navController)
     }
 }
+
+/**
+ * ViewModel
+ * LiveData
+ * Navigation Component
+ * Hilt
+ * ViewPager2
+ * Flow
+ *
+ * アーキテクチャ
+ * 責務を分離
+ * UI・ビジネスロジック・HTTP通信・DB触る場所
+ *
+ * MVC
+ * MODEL・VIEW・CONTROLLER
+ */
