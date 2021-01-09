0ebd1cda0ed20b9f881ec1c817422d0dc7f6d7ed
github lion-king-IT <reo.runningg@gmail.com>
2021-01-09 12:39:29 +0900

    Navigation実装して、インスタっぽくなった

--- app/src/main/java/com/reo/running/runnershigh/MainActivity.kt
+++ app/src/main/java/com/reo/running/runnershigh/MainActivity.kt
@@ -1,24 +1,11 @@
 package com.reo.running.runnershigh
-/**
- * 目的
- * ・とにかく、ランニングを習慣にしている人、あるいはしようとしている人がランニング出来るぞとワクワクするような工夫を凝らしたアプリを作る
- * ・時間や距離の数を何かに例えて成長実感を感じる仕組みを作る
- *
- * 処理概要具体化
- * 1月中
- * 下部ボタンを押すとUI切り替え
- * firebase 認証
- * firebase DBで走った距離と時間を管理
- * 2月中
- * 位置情報取得
- * 移動距離取得
- * 走っている時間タイマーを進める
- */
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +13,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        setupWit
+        setupWithNavController(bottom_navigation,navController)
     }
 }
