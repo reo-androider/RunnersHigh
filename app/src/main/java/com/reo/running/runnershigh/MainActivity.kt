640963f3afe42394bd9452741f6da967b2435b97
github lion-king-IT <reo.runningg@gmail.com>
2021-01-15 22:16:55 +0900

    marker

--- app/src/main/java/com/reo/running/runnershigh/MainActivity.kt
+++ app/src/main/java/com/reo/running/runnershigh/MainActivity.kt
@@ -3,15 +3,21 @@ package com.reo.running.runnershigh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.activity_main.*
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.reo.running.runnershigh.databinding.ActivityMainBinding
+import java.util.zip.Inflater
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        setupWithNavController(bottom_navigation,navController)
+        setupWithNavController(binding.bottomNavigation,navController)
     }
 }
--- app/src/main/java/com/reo/running/runnershigh/fragments/Fragment1.kt
+++ app/src/main/java/com/reo/running/runnershigh/fragments/Fragment1.kt
@@ -1,47 +1,124 @@
 package com.reo.running.runnershigh.fragments
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.inflate
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import com.reo.running.runnershigh.R
-import kotlinx.android.synthetic.main.fragment_1.*
-import java.util.zip.Inflater
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.reo.running.runnershigh.databinding.Fragment1Binding
 
-class Fragment1 : Fragment(){
+class Fragment1 : Fragment(),OnMapReadyCallback {
+    var map:GoogleMap? = null
+    private lateinit var binding: Fragment1Binding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-            super.onCreateView(inflater, container, savedInstanceState)
-            return inflater.inflate(R.layout.fragment_1,container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = Fragment1Binding.inflate(layoutInflater, container, false)
+        return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mapView.onCreate(savedInstanceState)
+        binding.mapView.onCreate(savedInstanceState)
+        context?.run {
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        }
+
+        //どのような取得方法を要求
+        val locationRequest = LocationRequest().apply {
+
+            //精度重視と省電力重視を両立するため2種類の更新間隔を指定
+            //今回は公式のサンプル通りにする
+            interval = 1                                   //最遅の更新間隔（ただし正確ではない）
+            fastestInterval = 1                              //最短の更新間隔
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY   //精度重視
+        }
+
+        //コールバック
+        val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                //更新直後の位置が格納されているはず
+                val location = locationResult?.lastLocation ?: return
+//                Toast.makeText(
+//                    context, "緯度:${location.latitude}" +
+//                            " 経度:${location.longitude}", Toast.LENGTH_LONG
+//                ).show()
+                //TODO currentLocationをonMapReadyに渡したい！
+                map?.addMarker(MarkerOptions()
+                        .position(LatLng(location.latitude,location.longitude))
+                        .visible(true))
+            }
+        }
+
+        //位置情報を更新
+        context?.run {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+
+
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback,Looper.myLooper())
+        }
     }
 
+    //Map上の現在地にマーカーを指す
+    override fun onMapReady(googleMap : GoogleMap) {
+        //map?.addMarker(MarkerOptions().position(location.latitude,location.))      //positionの引数に緯度と経度を渡したい！
+        map = googleMap
+    }
+
+//    緯度と経度を渡す
+//    private fun transfer(currentLocation:Array<Double>) {
+//
+//    }
 
     override fun onStart() {
         super.onStart()
-        mapView.onStart()
+        binding.mapView.onStart()
     }
 
     override fun onResume() {
         super.onResume()
-        mapView.onResume()
+        binding.mapView.onResume()
     }
 
     override fun onPause() {
         super.onPause()
-        mapView.onPause()
+        binding.mapView.onPause()
     }
-    /*
+/*
     override fun onDestroy() {
         super.onDestroy()
         mapView.onDestroy()
     }
-
-     */
-
+    */
 }
