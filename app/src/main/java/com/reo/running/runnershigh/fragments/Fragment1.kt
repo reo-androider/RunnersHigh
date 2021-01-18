6d0007fd5adab0a6d522cb7fa9cfb9e30c3115f4
github lion-king-IT <reo.runningg@gmail.com>
2021-01-18 12:26:17 +0900

    線の描画ができると、熱い！！！

--- app/src/main/java/com/reo/running/runnershigh/fragments/Fragment1.kt
+++ app/src/main/java/com/reo/running/runnershigh/fragments/Fragment1.kt
@@ -1,10 +1,10 @@
 package com.reo.running.runnershigh.fragments
 
 import android.Manifest
-import android.app.job.JobInfo
 import android.content.pm.PackageManager
-import android.location.Location
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
@@ -17,16 +17,15 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.*
 import com.google.maps.android.SphericalUtil
 import com.reo.running.runnershigh.databinding.Fragment1Binding
-import com.reo.running.runnershigh.Resource
-import java.util.*
 
 
 class Fragment1 : Fragment() {
-    var map:GoogleMap? = null
+   // var map:GoogleMap? = null
     private lateinit var binding: Fragment1Binding
     private lateinit var fusedLocationClient:FusedLocationProviderClient
     var distance:Double = 0.0
     var total:Double = 0.0
+   // var switch = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -65,39 +64,74 @@ class Fragment1 : Fragment() {
                 super.onLocationResult(locationResult)
                 //更新直後の位置が格納されているはず
                  val location = locationResult?.lastLocation ?: return
+                // TODO 3Dにする
+                // TODO 方角を自分が向いている方向にする
                 //マーカーを指した箇所の緯度経度を取って置く。
-                var pre = LatLng(location.latitude,location.longitude)
                     binding.mapView.getMapAsync{
-                        context?.run {
-                            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                                // TODO: Consider calling
-                                //    ActivityCompat#requestPermissions
-                                // here to request the missing permissions, and then overriding
-                                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                                //                                          int[] grantResults)
-                                // to handle the case where the user grants the permission. See the documentation
-                                // for ActivityCompat#requestPermissions for more details.
-                                 it.isMyLocationEnabled = true
-                            }
-
-                        }
+                        // zoom-in
+                        val zoomValue = 30.0f
+                        var latLng = LatLng(location.latitude,location.longitude)
+                        it.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoomValue)
+                        )
+                        //marker
                         it.addMarker(
                             MarkerOptions()
-                                .position(pre)
+                                .position(LatLng(location.latitude,location.longitude))
                         //        .icon(BitmapDescriptorFactory.fromBitmap(Resource.getBitmap(context, R.drawable.in_trace,)))
                         )
                         //it.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude,location.longitude),20f))
-                        it.uiSettings.isScrollGesturesEnabled = true    // 平行移動可能に
-                        it.uiSettings.isZoomGesturesEnabled = true      // 縮尺変更
+                        // 平行移動可能に
+                        it.uiSettings.isScrollGesturesEnabled = true
+                        // 縮尺変更
+                        it.uiSettings.isZoomGesturesEnabled = true
+                        // Marker押すとでてくるよ
                         it.uiSettings.isMapToolbarEnabled = true
-                        it.isMyLocationEnabled = true                   // 現在地ボタン
-                        it.isMyLocationEnabled = true                   // 現在地表示
+                        //コンパス
+                        it.uiSettings.isCompassEnabled = true
+                        // 現在地ボタン
+                        context?.run {
+                        if (ActivityCompat.checkSelfPermission(
+                                this,
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                                this,
+                                Manifest.permission.ACCESS_COARSE_LOCATION
+                            ) != PackageManager.PERMISSION_GRANTED
+                        ) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            it.isMyLocationEnabled = true
+                        }
+                        }
 
-                        var now = pre
-                        distance = SphericalUtil.computeDistanceBetween(pre, now)
-                        total += distance
-                        binding.distance.setText("$total")
+                        // 基準となる緯度・経度
+                        var stdLct = LatLng(location.latitude,location.longitude)
+
+                         //遅延処理
+//                        Looper.myLooper()?.let {
+//                            Handler(it).postDelayed({
+//                                // 最新の緯度経度
+//                                var newLct = LatLng(location.latitude,location.longitude)
+//                                distance = SphericalUtil.computeDistanceBetween(stdLct, newLct)
+//                                total += distance
+//                                                    },3000000)
+//                        }
+
+                        // 線の描画
+                        it.addPolyline(
+                            PolylineOptions()
+                                .add(stdLct)
+                                .add(LatLng(location.latitude,location.longitude))
+                                .color(Color.RED)
+                                .width(10f)
+                        )
+                      //  binding.meter.setText("$total")
+
                 }
             }
         }
