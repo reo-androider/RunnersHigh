1e43576571908a73f6a36a433fd923b0246678ae
github lion-king-IT <reo.runningg@gmail.com>
2021-01-26 08:03:56 +0900

    fragmentRunからDialogからのFragmentResultへの流れができた

--- app/src/main/java/com/reo/running/runnershigh/MainActivity.kt
+++ app/src/main/java/com/reo/running/runnershigh/MainActivity.kt
@@ -2,21 +2,27 @@ package com.reo.running.runnershigh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.reo.running.runnershigh.databinding.ActivityMainBinding
+import com.reo.running.runnershigh.databinding.Fragment2Binding
+import com.reo.running.runnershigh.fragments.Fragment2
 import java.util.zip.Inflater
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityMainBinding
+    val fragment = Fragment2()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         window.decorView.apply {
             // Hide both the navigation bar and the status bar.
@@ -29,5 +35,10 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         setupWithNavController(binding.bottomNavigation,navController)
 
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        return super.onTouchEvent(event)
     }
 }
---
+++ app/src/main/java/com/reo/running/runnershigh/MyGestureListener.kt
@@ -0,0 +1,9 @@
+package com.reo.running.runnershigh
+
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import com.reo.running.runnershigh.databinding.Fragment2Binding
+
+class MyGestureListener() : GestureDetector.SimpleOnGestureListener() {
+}
