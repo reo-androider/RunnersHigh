de37ec3990cb4093e8c7a9bded8582fff9e139fa
github lion-king-IT <reo.runningg@gmail.com>
2021-01-25 20:40:07 +0900

    resultFragmentへ遷移されない…

---
+++ app/src/main/java/com/reo/running/runnershigh/DialogMaker.kt
@@ -0,0 +1,32 @@
+package com.reo.running.runnershigh
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import java.lang.IllegalStateException
+
+class DialogMaker:DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage("ランニングを終了しますか？")
+                    .setPositiveButton("YES",
+                    DialogInterface.OnClickListener{ dialog, id ->
+                        findNavController().navigate(R.id.action_dialogMaker_to_fragmentResult)
+                    })
+                    .setNegativeButton("CANCEL",
+                            DialogInterface.OnClickListener{ dialog, which ->
+                                dialog.dismiss()
+                            })
+            builder.create()
+        }?:throw IllegalStateException("Activity cannot be null")
+    }
+}
--- app/src/main/java/com/reo/running/runnershigh/fragments/FragmentGO.kt
+++ app/src/main/java/com/reo/running/runnershigh/fragments/FragmentGO.kt
@@ -10,20 +10,21 @@ import com.reo.running.runnershigh.R
 import com.reo.running.runnershigh.databinding.FragmentGoBinding
 
 class FragmentGO : Fragment() {
-    private lateinit var binding:FragmentGoBinding
+    private lateinit var binding: FragmentGoBinding
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        binding = FragmentGoBinding.inflate(layoutInflater,container,false)
+        binding = FragmentGoBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        binding.start.setOnClickListener{
-//          findNavController().navigate(R.id.action_navi_smile_to_fragment12)
-//        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.start.setOnClickListener {
+            //findNavController().navigate(R.id.action_navi_smile_to_fragment12)
+        }
     }
+}
