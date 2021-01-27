d8714009fc85eccd2b67bf0830da911053dbc980
github lion-king-IT <reo.runningg@gmail.com>
2021-01-27 22:50:23 +0900

    READ関数を使った！

--- app/src/main/java/com/reo/running/runnershigh/User.kt
+++ app/src/main/java/com/reo/running/runnershigh/User.kt
@@ -5,11 +5,4 @@ import androidx.room.Entity
 import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
-@Entity
-data class User(
-    @PrimaryKey(autoGenerate = true) val uid: Int,
-    @ColumnInfo(name="first_name") val todoTitle: String?,
-    @ColumnInfo(name = "last_name") val lastName: String?
-    ) {
 
-}
