f1259555a71de0362198eba96f9d8f431597f123
github lion-king-IT <reo.runningg@gmail.com>
2021-01-27 12:46:09 +0900

    インスタ(ROOMのインスタンス呼び出す必要がある)

--- app/src/main/java/com/reo/running/runnershigh/AppDatabase.kt
+++ app/src/main/java/com/reo/running/runnershigh/AppDatabase.kt
@@ -1,8 +1,9 @@
 package com.reo.running.runnershigh
 
 import androidx.room.*
+import com.twitter.sdk.android.core.models.User
 
-@Database(entities = arrayOf(AddressDao::class),version = 1)
+@Database(entities = arrayOf(User::class),version = 1)
 abstract class AppDatabase:RoomDatabase() {
-    abstract fun adderssDao(): AddressDao
+    abstract fun userDao(): UserDao
 }
