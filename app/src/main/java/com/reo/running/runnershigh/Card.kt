bb1df0db9019bbd83e4039fec62e61f94bd08821
github lion-king-IT <reo.runningg@gmail.com>
2021-01-28 23:14:01 +0900

    ROOMのREAD機能と余分なコメント削除

---
+++ app/src/main/java/com/reo/running/runnershigh/Card.kt
@@ -0,0 +1,11 @@
+package com.reo.running.runnershigh
+
+class Card(colorName: String, colorCode: String) {
+    var colorCode = ""
+    var colorName = ""
+
+    init {
+        this.colorCode = colorCode
+        this.colorName = colorName
+    }
+}
