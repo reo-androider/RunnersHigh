9ae7220135d440f9f8b339391813e4594be3d235
github lion-king-IT <reo.runningg@gmail.com>
2021-02-01 19:36:38 +0900

    GPS待ち画面を改善した

--- app/src/main/java/com/reo/running/runnershigh/MainActivity.kt
+++ app/src/main/java/com/reo/running/runnershigh/MainActivity.kt
@@ -24,14 +24,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        window.decorView.apply {
-            // Hide both the navigation bar and the status bar.
-            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-            // a general rule, you should design your app to hide the status bar whenever you
-            // hide the navigation bar.
-            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
-        }
-
         val navController = findNavController(R.id.nav_host_fragment)
         setupWithNavController(binding.bottomNavigation,navController)
 
