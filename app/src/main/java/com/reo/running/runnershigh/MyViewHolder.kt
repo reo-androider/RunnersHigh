2f36d33ac99ee048cff9b97e7e12ea1e1d05b84e
github lion-king-IT <reo.runningg@gmail.com>
2021-02-04 13:55:14 +0900

    drawableを横並びにするrecyclerView実装出来ました！

--- app/src/main/java/com/reo/running/runnershigh/MyViewHolder.kt
+++ app/src/main/java/com/reo/running/runnershigh/MyViewHolder.kt
@@ -1,11 +1,10 @@
 package com.reo.running.runnershigh
 
-import android.media.Image
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val courseImage: ImageView  = itemView.findViewById(R.id.park1)
+    val course: ImageView = itemView.findViewById(R.id.park1)
 }
