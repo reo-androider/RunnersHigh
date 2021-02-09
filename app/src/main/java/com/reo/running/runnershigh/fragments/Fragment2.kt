ceb6618f19e93da911f3b9870b2af78b5cb34255
github lion-king-IT <reo.runningg@gmail.com>
2021-02-10 08:22:58 +0900

    更新の不具合を手動で修正

--- app/src/main/java/com/reo/running/runnershigh/fragments/Fragment2.kt
+++ app/src/main/java/com/reo/running/runnershigh/fragments/Fragment2.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import android.view.*
 import androidx.core.view.GestureDetectorCompat
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.charts.LineChart
 import com.reo.running.runnershigh.R
 import com.reo.running.runnershigh.databinding.Fragment2Binding
 
 class Fragment2 : Fragment() {
     private lateinit var binding: Fragment2Binding
+    private lateinit var mLineChar:LineChart
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?
+    ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.fragment_2, container, false)
+        binding = Fragment2Binding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
 }
