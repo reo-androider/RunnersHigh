1d6dd7235f293f1536929e7a36e426a3c24950fa
github lion-king-IT <reo.runningg@gmail.com>
2021-02-09 11:40:22 +0900

    リザルト画面のランニング結果を二つ目のテーブルで管理できるようになりました。

--- app/src/main/java/com/reo/running/runnershigh/RecordDatabase.kt
+++ app/src/main/java/com/reo/running/runnershigh/RecordDatabase.kt
@@ -3,22 +3,39 @@ package com.reo.running.runnershigh
 import android.graphics.Bitmap
 import androidx.room.*
 
-@Database(entities = [Record::class], version = 1)
+@Database(entities = arrayOf(
+    Record::class,
+    Record2::class) ,version = 1)
 @TypeConverters(BitmapConverter::class)
 abstract class RecordDatabase : RoomDatabase() {
     abstract fun recordDao(): RecordDao
+    abstract fun recordDao2(): RecordDao2
 }
 
 @Entity
 data class Record(
-        @PrimaryKey(autoGenerate = true)
-        val id: Int,
-        val time: String,
-        val distance: Double,
-        val calorie: Int,
-        val runDate: String,
-        val bitmap: Bitmap?,
-        val takenPhoto: Boolean
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val time: String,
+    val distance: Double,
+    val calorie: Int,
+    val runDate: String,
+    val bitmap: Bitmap?,
+    val takenPhoto: Boolean
+)
+
+@Entity
+data class Record2(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val bitmap: Bitmap?,
+    val time: String,
+    val distance: Double,
+    val calorie: Int,
+    val runData: String,
+    val colorId: String,
+    val revaluationMark: String,
+    val memo: String
 )
 
 @Dao
@@ -37,5 +54,25 @@ interface RecordDao {
 
     @Delete
     suspend fun deleteRecord(record: Record)
+
+}
+
+
+@Dao
+interface RecordDao2 {
+    @Query("Select * From record2")
+    fun getAll2(): List<Record2>
+
+    @Query("select * From record2 Where id = :id")
+    suspend fun getRecord2(id: Int): Record2
+
+    @Insert
+    suspend fun insertRecord2(record: Record2)
+
+    @Update
+    suspend fun updateRecord2(record: Record2)
+
+    @Delete
+    suspend fun deleteRecord2(record: Record2)
 
 }
--- app/src/main/java/com/reo/running/runnershigh/ResultDataModel.kt
+++ app/src/main/java/com/reo/running/runnershigh/ResultDataModel.kt
@@ -3,11 +3,12 @@ package com.reo.running.runnershigh
 import android.graphics.Bitmap
 import androidx.room.PrimaryKey
 import io.realm.RealmObject
+import io.realm.annotations.Required
 
 open class ResultDataModel (
-    @PrimaryKey
-    var memo: String = "",
-    var feedBack: String = "",
-    var colorId:String = "",
+    @PrimaryKey open var id : Int = 0,
+    @Required open var memo: String = "",
+    open var feedBack: String = "",
+    open var colorId:String = "",
 //    var bitmap: Bitmap? = null
 ) : RealmObject()
