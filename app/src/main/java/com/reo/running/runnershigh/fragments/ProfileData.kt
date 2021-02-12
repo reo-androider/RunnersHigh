f94a5ef45e0512f40b3dc1c34ee85e451365a069
github lion-king-IT <reo.runningg@gmail.com>
2021-02-12 20:19:51 +0900

    profileFragmentの変更点をマージする用

---
+++ app/src/main/java/com/reo/running/runnershigh/fragments/ProfileData.kt
@@ -0,0 +1,5 @@
+package com.reo.running.runnershigh.fragments
+
+import android.graphics.Bitmap
+
+data class ProfileData(val bitmap: Bitmap?,val firstName:String?,val familyName:String?,val objective:String?,val weight:Float?)
--- app/src/main/java/com/reo/running/runnershigh/runData.kt
+++ app/src/main/java/com/reo/running/runnershigh/runData.kt
@@ -2,4 +2,4 @@ package com.reo.running.runnershigh
 
 import android.graphics.Bitmap
 
-data class runData ( val feedBack: String, val bitmap: Bitmap?, val memo: String , val colorId: Int)
+data class runData( val feedBack: String, val bitmap: Bitmap?, val memo: String , val colorId: Int)
