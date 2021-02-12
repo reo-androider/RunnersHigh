148fe96fd59de8ce9dd0eec984fcce8cffd6eae6
github lion-king-IT <reo.runningg@gmail.com>
2021-02-12 18:00:42 +0900

    プロフィール設定画面完成

--- app/src/main/java/com/reo/running/runnershigh/fragments/FragmentProfileSetting.kt
+++ app/src/main/java/com/reo/running/runnershigh/fragments/FragmentProfileSetting.kt
@@ -7,24 +7,27 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.reo.running.runnershigh.R
+import com.reo.running.runnershigh.databinding.ActivityLoginBinding.bind
+import com.reo.running.runnershigh.databinding.ActivityLoginBinding.inflate
 import com.reo.running.runnershigh.databinding.FragmentProfileBinding
+import com.reo.running.runnershigh.databinding.FragmentProfileSettingBinding
+import javax.sql.RowSet
 
-class FragmentProfile : Fragment(){
-    private lateinit var binding: FragmentProfileBinding
-
+class FragmentProfileSetting : Fragment() {
+    private lateinit var binding:FragmentProfileSettingBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        super.onCreateView(inflater, container, savedInstanceState)
-        binding = FragmentProfileBinding.inflate(layoutInflater,container,false)
+        binding = FragmentProfileSettingBinding.inflate(layoutInflater,container,false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.run {
-            settingImage.setOnClickListener {
-                findNavController().navigate(R.id.action_navi_setting_to_fragmentProfileSetting)
+            profileBack.setOnClickListener {
+                findNavController().navigate(R.id.action_fragmentProfileSetting_to_navi_setting2)
             }
+
+
         }
     }
 }
