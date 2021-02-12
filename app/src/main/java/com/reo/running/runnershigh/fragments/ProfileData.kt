c515b118ff8cd68db8f645b805d45a65b584e409
github lion-king-IT <reo.runningg@gmail.com>
2021-02-12 20:28:45 +0900

    profileFragmentの変更点をマージしました

--- app/src/main/java/com/reo/running/runnershigh/fragments/ProfileData.kt
+++ app/src/main/java/com/reo/running/runnershigh/fragments/ProfileData.kt
@@ -2,4 +2,10 @@ package com.reo.running.runnershigh.fragments
 
 import android.graphics.Bitmap
 
-data class ProfileData(val bitmap: Bitmap?,val firstName:String?,val familyName:String?,val objective:String?,val weight:Float?)
+data class ProfileData(
+    val bitmap: Bitmap? = null,
+    val firstName:String = "あなたの",
+    val familyName:String = "名前",
+    val objective:String = "",
+    val weight:Float = 60f
+    )
