f34f75799bf68352f0096bab8b6b4d834a4680c8
github lion-king-IT <reo.runningg@gmail.com>
2021-02-12 10:14:41 +0900

    プロフィールフラグメントのUI完成

---
+++ app/src/main/java/com/reo/running/runnershigh/PagerAdapter.kt
@@ -0,0 +1,10 @@
+package com.reo.running.runnershigh
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.appbar.AppBarLayout
+
+//class PagerAdapter(fm:FragmentManager,private val fragmentList: List<Fragment>):
+//    FragmentStateAdapter(fm,){
+//}
