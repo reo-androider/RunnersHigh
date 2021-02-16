67b2013f2687e83cbf40ab9a82c955238fc21f92
github lion-king-IT <reo.runningg@gmail.com>
2021-02-17 07:03:13 +0900

    写真をcloudへ保存したしたい

---
+++ app/src/main/java/com/reo/running/runnershigh/MyViewHolder2.kt
@@ -0,0 +1,11 @@
+package com.reo.running.runnershigh
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyViewHolder2(itemView:View) : RecyclerView.ViewHolder(itemView) {
+    val runPhoto: ImageView = itemView.findViewById(R.id.runPhoto)
+//    val runDate : TextView = itemView.findViewById(R.id.run_date)
+}
--- app/src/main/java/com/reo/running/runnershigh/fragments/FragmentPhoto.kt
+++ app/src/main/java/com/reo/running/runnershigh/fragments/FragmentPhoto.kt
@@ -6,12 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.reo.running.runnershigh.MyAdapter2
 import com.reo.running.runnershigh.MyApplication
+import com.reo.running.runnershigh.Record2
 import com.reo.running.runnershigh.databinding.FragmentPhotoBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class FragmentPhoto : Fragment() {
     private lateinit var binding:FragmentPhotoBinding
     private val readDao = MyApplication.db.recordDao2()
+    val photo = listOf<Record2>()
+    private var i = 0
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentPhotoBinding.inflate(layoutInflater,container,false)
         return  binding.root
@@ -20,9 +29,15 @@ class FragmentPhoto : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.run {
-            val photo = listOf<Bitmap>(
+            lifecycleScope.launch(Dispatchers.IO) {
+//                val db = readDao.getAll2
 
-            )
+                withContext(Dispatchers.Main) {
+
+                }
+            }
+//            mainRecyclerView.adapter = MyAdapter2(photo)
+            mainRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         }
     }
 }
