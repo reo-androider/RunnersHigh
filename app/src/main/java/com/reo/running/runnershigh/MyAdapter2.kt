eb0f8ec1b4494e72f6d2f6c8d14ee8e324f0e1ca
github lion-king-IT <reo.runningg@gmail.com>
2021-02-17 07:03:13 +0900

    写真をcloudへ保存したしたい

--- app/src/main/java/com/reo/running/runnershigh/MyAdapter2.kt
+++ app/src/main/java/com/reo/running/runnershigh/MyAdapter2.kt
@@ -1,4 +1,18 @@
 package com.reo.running.runnershigh
 
-class MyAdapter2  {
+import android.graphics.Bitmap
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter2(val list: List<Bitmap?>) : RecyclerView.Adapter<MyViewHolder2>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder2 {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_photo_item,parent,false)
+        return MyViewHolder2(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder2, position: Int) {
+        holder.runPhoto.setImageBitmap(list[position])
+    }
+    override fun getItemCount(): Int = list.size
 }
