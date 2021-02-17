347592289c35e5fcd882e44a5ba927da771a2e96
github lion-king-IT <reo.runningg@gmail.com>
2021-02-17 12:48:07 +0900

    グラフをRecyclerViewで表示させることに

--- app/src/main/java/com/reo/running/runnershigh/fragments/FragmentGraph.kt
+++ app/src/main/java/com/reo/running/runnershigh/fragments/FragmentGraph.kt
@@ -1,6 +1,7 @@
 package com.reo.running.runnershigh.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -18,10 +19,11 @@ import kotlinx.coroutines.withContext
 
 class FragmentGraph : Fragment() {
     private lateinit var binding: FragmentGraphBinding
-    private val runDatabase = MyApplication.db.recordDao2()
     private var entries:ArrayList<Entry> = ArrayList()
     private var lastId = 0
     private var totalDistance = 0.0
+    private val runDatabase = MyApplication.db.recordDao2()
+    private var totalCalorie = 0.0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
@@ -35,30 +37,24 @@ class FragmentGraph : Fragment() {
             (activity as MainActivity).binding.bottomNavigation.visibility =
                 View.VISIBLE
             lifecycleScope.launch(Dispatchers.IO) {
-
                 val read = runDatabase.getAll2()
-
                 val xValue = mutableListOf<String>()
                 lastId = read.last().id - 1
                 for (i in 0..lastId) {
                     xValue.add(read[i].runData)
                 }
-
                 val yValue = mutableListOf<Double>()
                 for (i in 0..lastId) {
                     yValue.add((read[i].distance) + totalDistance)
                     totalDistance += read[i].distance
                 }
-
                 withContext(Dispatchers.Main) {
                     for (i in 0..lastId) {
                         entries.add(i, Entry(mConverter(yValue[i]),i))
                     }
                     val dataset = LineDataSet(entries, "")
-
                     val data = LineData(xValue, dataset)
                     dataset.setColors(ColorTemplate.COLORFUL_COLORS)
-
                     val leftAxis = binding.lineChart.getAxisLeft().run {
                         axisMinValue = 0f
                         when {
@@ -84,6 +80,63 @@ class FragmentGraph : Fragment() {
                         invalidate()
                         this.data = data
                         animateY(1)
+                    }
+                }
+            }
+
+            lifecycleScope.launch(Dispatchers.IO) {
+                totalCalorie.let {
+
+                    val read = runDatabase.getAll2()
+                    Log.d("ROOM","${read}")
+
+                    val xValue = mutableListOf<String>()
+                    lastId = read.last().id - 1
+                    for (i in 0..lastId) {
+                        xValue.add(read[i].runData)
+                    }
+
+                    val yValue = mutableListOf<Double>()
+                    for (i in 0..lastId) {
+                        yValue.add((read[i].calorie) + totalCalorie)
+                        Log.d("ROOM","${yValue[i]}")
+                        totalCalorie += read[i].calorie
+                    }
+
+                    withContext(Dispatchers.Main) {
+                        for (i in 0..lastId) {
+                            entries.add(i, Entry(yValue[i].toFloat(), i))
+                        }
+                        val dataset = LineDataSet(entries, "")
+
+                        val data = LineData(xValue, dataset)
+                        dataset.setColors(ColorTemplate.COLORFUL_COLORS)
+
+                        val leftAxis = binding.lineChart2.getAxisLeft().run {
+                            axisMinValue = 0f
+                            when {
+                                it == 0.0 -> axisMaxValue = 100f
+                                it > 100.0 -> axisMaxValue = 500f
+                                it > 500.0 -> axisMaxValue = 1000f
+                                it > 1000.0 -> axisMaxValue = 2000f
+                                it > 2000.0 -> axisMaxValue = 5000f
+                                it > 5000.0 -> axisMaxValue = 10000f
+                                it > 10000.0 -> axisMaxValue = 10000000f
+                            }
+                            valueFormatter = DistanceUnit()
+                        }
+
+                        val rightAxis = binding.lineChart2.getAxisRight().run {
+                            isEnabled = false
+                            setDrawLabels(false)
+                            setDrawGridLines(true)
+                        }
+
+                        lineChart2.run {
+                            invalidate()
+                            this.data = data
+                            animateY(1)
+                        }
                     }
                 }
             }
