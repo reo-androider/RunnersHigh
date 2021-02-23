fb83b94ee382638828fb950363cd0c7da193e3df
github lion-king-IT <reo.runningg@gmail.com>
2021-02-23 13:09:11 +0900

    MyAdapter内にスコ―プ関数を追加
    (cherry picked from commit ff1a6934388ceb45eb8ea4062717fe8fd057ddd3)

--- app/src/main/java/com/reo/running/runnershigh/MyAdapter2.kt
+++ app/src/main/java/com/reo/running/runnershigh/MyAdapter2.kt
@@ -1,7 +1,5 @@
 package com.reo.running.runnershigh
 
-import android.content.res.Resources
-import android.graphics.Bitmap
 import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
@@ -16,51 +14,50 @@ class MyAdapter2(val list: List<Record2>) : RecyclerView.Adapter<MyViewHolder2>(
     }
 
     override fun onBindViewHolder(holder: MyViewHolder2, position: Int) {
-        //色をセットする
-        holder.runPhoto.run {
-            setImageBitmap(list[position].bitmap)
-            if (list[position].colorId != "") {
-                setBackgroundColor(Color.parseColor(list[position].colorId))
+        holder.run {
+            runPhoto.run {
+                setImageBitmap(list[position].bitmap)
+                if (list[position].colorId.isNotEmpty()) {
+                    setBackgroundColor(Color.parseColor(list[position].colorId))
+                }
+            }
+            runDate.run {
+                setText(list[position].runData)
+            }
+            runTime.run {
+                setText(list[position].time)
+            }
+            runDistance.run {
+                text = "${list[position].distance}km"
+            }
+            runCalorie.run {
+                text = "${list[position].calorie}kcal"
+            }
+            runMemo.text = "${list[position].memo}"
 
-            }        }
-        holder.runDate.run {
-            setText(list[position].runData)
-            Log.d("color","${list[position].colorId}")
-//            setTextColor(Color.parseColor(list[position].colorId))
-        }
-        holder.runTime.run {
-            setText(list[position].time)
-//            setTextColor(Color.parseColor(list[position].colorId))
-        }
-        holder.runDistance.run {
-            text = "${list[position].distance}km"
-//            setTextColor(Color.parseColor(list[position].colorId))
-        }
-        holder.runCalorie.run {
-            text = "${list[position].calorie}kcal"
-//            setTextColor(Color.parseColor(list[position].colorId))
-        }
-        holder.runMemo.text = "${list[position].memo}"
-        when(list[position].revaluationMark) {
-            "perfect" -> {
-                holder.runFeedBack.setImageResource(R.drawable.ic_perfect)
-                holder.runFeedBack.visibility = View.VISIBLE
-            }
-            "good" -> {
-                holder.runFeedBack.setImageResource(R.drawable.ic_good)
-                holder.runFeedBack.visibility = View.VISIBLE
-            }
-            "soso" -> {
-                holder.runFeedBack.setImageResource(R.drawable.ic_soso)
-                holder.runFeedBack.visibility = View.VISIBLE
-            }
-            "bad" -> {
-                holder.runFeedBack.setImageResource(R.drawable.ic_bad)
-                holder.runFeedBack.visibility = View.VISIBLE
-            }
-            "tooBad" -> {
-                holder.runFeedBack.setImageResource(R.drawable.ic_sick)
-                holder.runFeedBack.visibility = View.VISIBLE
+            runFeedBack.run {
+                when (list[position].revaluationMark) {
+                    "perfect" -> {
+                        setImageResource(R.drawable.ic_perfect)
+                        visibility = View.VISIBLE
+                    }
+                    "good" -> {
+                        setImageResource(R.drawable.ic_good)
+                        visibility = View.VISIBLE
+                    }
+                    "soso" -> {
+                        setImageResource(R.drawable.ic_soso)
+                        visibility = View.VISIBLE
+                    }
+                    "bad" -> {
+                        setImageResource(R.drawable.ic_bad)
+                        visibility = View.VISIBLE
+                    }
+                    "tooBad" -> {
+                        setImageResource(R.drawable.ic_sick)
+                        visibility = View.VISIBLE
+                    }
+                }
             }
         }
     }
