f943a56179850b3bfe06d5bd5f4c77574f51edc9
github lion-king-IT <reo.runningg@gmail.com>
2021-02-24 10:02:08 +0900

    CalorieUnit内の無駄なオーバーライドを削除

--- app/src/main/java/com/reo/running/runnershigh/CalorieUnit.kt
+++ app/src/main/java/com/reo/running/runnershigh/CalorieUnit.kt
@@ -6,12 +6,8 @@ import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.formatter.YAxisValueFormatter
 import com.github.mikephil.charting.utils.ViewPortHandler
 
-class CalorieUnit : ValueFormatter, YAxisValueFormatter {
+class CalorieUnit : YAxisValueFormatter {
     override fun getFormattedValue(value: Float, yAxis: YAxis?): String {
         return value.toInt().toString() + "kcal"
-    }
-
-    override fun getFormattedValue(value: Float, entry: Entry?, dataSetIndex: Int, viewPortHandler: ViewPortHandler?): String {
-        TODO("Not yet implemented")
     }
 }
