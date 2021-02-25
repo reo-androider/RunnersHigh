804a34742fbe85ce18261602cdfcf113582f37cd
github lion-king-IT <reo.runningg@gmail.com>
2021-02-25 19:06:11 +0900

    元の画像を変更して、サイズの決め打ちを解決する方法を発見

--- app/src/main/java/com/reo/running/runnershigh/paintsViewHolder.kt
+++ app/src/main/java/com/reo/running/runnershigh/paintsViewHolder.kt
@@ -5,5 +5,5 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 
 class paintsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val course: ImageView = itemView.findViewById(R.id.park1)
+    val course: ImageView = itemView.findViewById(R.id.paints)
 }
