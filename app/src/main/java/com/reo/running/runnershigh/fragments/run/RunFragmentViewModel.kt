d1f29b9d48f6516f4997a95107ef2e14f8f17fa1
github lion-king-IT <reo.runningg@gmail.com>
2021-03-10 10:10:13 +0900

    ViewModelを導入したい

--- app/src/main/java/com/reo/running/runnershigh/fragments/run/RunFragmentViewModel.kt
+++ app/src/main/java/com/reo/running/runnershigh/fragments/run/RunFragmentViewModel.kt
@@ -1,4 +1,24 @@
 package com.reo.running.runnershigh.fragments.run
 
-class RunFragmentViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class RunFragmentViewModel : ViewModel() {
+
+    companion object {
+        private const val RUN_STATE_BEFORE = 3001
+        private const val RUN_STATE_START = 3002
+        private const val RUN_STATE_PAUSE = 3003
+    }
+
+    private val _runState = MutableLiveData<Int>()
+    val runState: LiveData<Int>
+    get() = _runState
+
+    init {
+        _runState.value = RUN_STATE_BEFORE
+    }
+
+
 }
